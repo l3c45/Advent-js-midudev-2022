db6f85e3d14ce7210559080bc97add7bbfdab512
lucas <mrlucaszarate@gmail.com>
2022-12-24 15:45:28 -0300

    Ejercicio 20 resuelto

--- challengues/day20.js
+++ challengues/day20.js
@@ -76,19 +76,30 @@ const gifts = [
 // //     { type: 'Gasoline', num: 3 },
 // //     { type: 'Diesel', num: 5 }
 function howManyReindeers(reindeerTypes, gifts) {
-  const a = gifts.map((pais) => {
-    let tempWeigth=pais.weight
-  return   reindeerTypes.reduce((equipo, reno, i, arr) => {
-      if (reno.weightCapacity <= pais.weight) {
-        if(tempWeigth>reno.weightCapacity){
-          equipo.push(reno);
-        tempWeigth-reno.weightCapacity
-        return equipo;}
-      } else {
-        return equipo;
-      }
-    }, []);
-  });
+  return gifts.map((gift) => {
+    let max = gift.weight;
+    let reindeers = reindeerTypes
+      .map((x) => [x.type, x.weightCapacity])
+      .filter((x) => x[1] < max)
+      .sort((a, b) => a[1] - b[1]); // Menor a Mayor
 
-  return a;
+    let res = reindeers.map(([type]) => ({
+      type,
+      num: 0,
+    }));
+
+    reindeers.map((_, i) => {
+      let sliced = reindeers.slice(0, reindeers.length - i)
+      let sum = sliced.reduce((sum, e) => sum + e[1], 0);
+      sliced.map((_, i) => {
+        res[i].num += Math.floor(max / sum);
+      });
+      max %= sum;
+    });
+
+    return {
+      country: gift.country,
+      reindeers: res.reverse(),
+    };
+  });
 }
